58abe8aea58f6986a6dcd83def9e0760942cae5d
José Roberto Cundari Junior <88779658+JuniorCundari@users.noreply.github.com>
2021-10-21 22:20:41 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const LinksSocialMedia = {
+  github: 'JuniorCundari',
+  linkedin: 'in/josé-roberto-cundari-junior-a42961129/',
+  facebook: 'profile.php?id=100009284249220',
+  instagram: 'juniiorcundari'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userLogin.textContent = data.login
+      userImage.src = data.avatar_url
+    })
+}
+
+getGitHubProfileInfos()
